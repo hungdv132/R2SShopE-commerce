4934f7cadf8d4eb29893fc448181c6b54aa1b2d3
Giang Dinh Hoang <dhgiang85@gmail.com>
2023-12-15 10:05:00 +0700

    add swagger-ui

--- src/main/java/org/hurc/cms/config/SpringSecurityConfig.java
+++ src/main/java/org/hurc/cms/config/SpringSecurityConfig.java
@@ -29,7 +29,7 @@ public class SpringSecurityConfig  {
       "/user/home",
       "/uploads/**",
       "/assets/**",
-      "/"
+      "/**"
   };
   private JwtAuthenticationEntryPoint authenticationEntryPoint;
 
